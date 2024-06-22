fa0118dc9521ce8e05e2a970bdf1e7897a8cb3ad
ahmad786-code <ahmadrazadev2@gmail.com>
2024-06-22 19:42:42 +0500

    valid email

--- src/components/Subscriber.js
+++ src/components/Subscriber.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './subscribe.css'
 const Subscriber = () => {
+    const [email, setEmail] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+        setIsValid(validateEmail(email));
+      }, [email]);
+
+      
+    const handleChange = (e) => {
+        const emailValue = e.target.value;
+        setEmail(emailValue);
+        setIsValid(validateEmail(emailValue));
+    };
+
+    const validateEmail = (email) => {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    };
     return (
         <form className='subscribe'>
-            <input type="text" placeholder='Subscribe' className='subscribe_input' />
-            <input type="button" value="Subscribe" className='subscribe_button' />
+            <input type="email" placeholder='example@gmail.com' className='subscribe_input' value={email}
+                onChange={handleChange} />
+            <input type="button" value="Subscribe" className='subscribe_button' disabled={!isValid} />
         </form>
     )
 }
